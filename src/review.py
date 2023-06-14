c285478b6235c5642a8b649a4c69b105a757e31d
Kielson Zinn da Silva <kielsonzinn@gmail.com>
2023-06-14 08:37:56 -0300

    Initial commit

---
+++ src/review.py
@@ -0,0 +1,18 @@
+def review(config):
+    path_target = config['path_target']
+    path_source = config['path_source']
+
+    merge = config['merge']
+    project_id = merge['project_id']
+    merge_request_id = merge['merge_request_id']
+    
+    comments = []
+    
+    # TODO IMPLEMENTAR EXTENSION
+    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
+    #  {
+    #      "id": "",
+    #      "comment": ""
+    #  }
+
+    return comments
